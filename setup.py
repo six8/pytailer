bdbb7da824f118f45eac20c6fdec08d9ddb0526d
Ilya Kulakov <kulakov.ilya@gmail.com>
2015-08-05 15:01:25 -0400

    Fix PEP8 warnings in setup.py

--- setup.py
+++ setup.py
@@ -1,41 +1,37 @@
 from distutils.core import setup
 
-def main():
 
-    setup(
-        name = 'tailer',
-        packages=['tailer'],
-        package_dir = {'':'src'},
-        version = open('VERSION.txt').read().strip(),
-        author='Mike Thornton',
-        author_email='six8@devdetails.com',
-        url='http://github.com/six8/pytailer',
-        download_url='http://github.com/six8/pytailer',        
-        license='MIT',
-        keywords=['tail', 'head'],
-        description='Python tail is a simple implementation of GNU tail and head.',
-        classifiers = [
-            "Programming Language :: Python",
-            "Development Status :: 3 - Alpha",
-            "Environment :: Console",
-            "Intended Audience :: Developers",
-            "Intended Audience :: System Administrators",
-            "Operating System :: POSIX",
-            "License :: OSI Approved :: MIT License",
-            "Natural Language :: English",
-            'Topic :: System :: Logging',
-            'Topic :: Text Processing',
-            "Topic :: Software Development :: Libraries :: Python Modules",
-            "Topic :: System :: System Shells",
-            "Topic :: System :: Systems Administration",
+setup(
+    name='tailer',
+    packages=['tailer'],
+    package_dir={'': 'src'},
+    version=open('VERSION.txt').read().strip(),
+    author='Mike Thornton',
+    author_email='six8@devdetails.com',
+    url='http://github.com/six8/pytailer',
+    download_url='http://github.com/six8/pytailer',
+    license='MIT',
+    keywords=['tail', 'head'],
+    description='Python tail is a simple implementation of GNU tail and head.',
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: POSIX",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        'Topic :: System :: Logging',
+        'Topic :: Text Processing',
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: System Shells",
+        "Topic :: System :: Systems Administration",
+    ],
+    long_description=open('README.rst').read(),
+    entry_points={
+        'console_scripts': [
+            'pytail = tailer:main',
         ],
-        long_description=open('README.rst').read(),
-        entry_points = {
-            'console_scripts': [
-                'pytail = tailer:main',
-            ],
-        },        
-    )
-
-if __name__ == '__main__':
-    main()
+    },
+)
